ba0ff319ca422763b7fa3c2a5e89ffa902353077
Ravindra Pandey <ravindrapandey99999@gmail.com>
2024-02-07 17:19:15 +0530

    Test Login credentials Feature added

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,22 +10,33 @@ import {
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import Loader from "./Loader";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [isError, setIsError] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
 
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
 
+  const handleResetError = () => {
+    setTimeout(() => {
+      setIsError(null); // Resetting error after 1 second
+    }, 2000);
+  };
+
   const handleValidation = () => {
     const check = validate(email.current.value, password.current.value);
     setIsError(check);
-    if (check) return;
+    if (check) {
+      handleResetError();
+      return;
+    }
 
+    setIsLoading(true);
     if (!isSignInForm) {
       createUserWithEmailAndPassword(
         auth,
@@ -65,11 +76,17 @@ const Login = () => {
         password.current.value
       )
         .then((userCredential) => {
+          setIsLoading(false);
           const user = userCredential.user;
         })
         .catch((error) => {
+          setIsLoading(false);
           const errorCode = error.code;
           setIsError(errorCode);
+          if (errorCode) {
+            handleResetError();
+            return;
+          }
         });
     }
   };
@@ -78,8 +95,35 @@ const Login = () => {
     setIsSignInForm(!isSignInForm);
   };
 
+  const handleTestCredentials = () => {
+    setIsLoading(true);
+    const testEmail = "bohotKhaasLog@gmail.com";
+    const testPassword = "bohotKhaasLog@123";
+
+    email.current.value = null;
+    password.current.value = null;
+    email.current.value = testEmail;
+    password.current.value = testPassword;
+    
+    signInWithEmailAndPassword(auth, testEmail, testPassword)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        const errorCode = error.code;
+        setIsError(errorCode);
+        if (isError !== null) {
+          handleResetError();
+          return;
+        }
+      });
+  };
+
   return (
     <div className="relative flex justify-center items-center h-screen">
+      {isLoading && <Loader />}
       <Header />
       <div className="brightness-50 fixed -z-10 overflow-hidden">
         <img
@@ -116,13 +160,24 @@ const Login = () => {
           type="password"
           placeholder="Password"
         />
-        <p className="text-red-500 my-3 h-6 text-sm text-center">{isError}</p>
+        <p className={`text-red-500 my-3 h-6 text-sm text-center`}>{isError}</p>
         <button
           onClick={handleValidation}
-          className="bg-red-600 contrast-125 rounded-[4px] sm:w-72 w-auto py-3"
+          className="bg-red-600 contrast-125 scale-100 text-shadow active:scale-95 rounded-[4px] sm:w-72 w-auto py-3"
         >
           {isSignInForm ? "Sign in" : "Sign up"}
         </button>
+        {isSignInForm && (
+          <div>
+            <p className="text-center my-3">OR</p>
+            <button
+              onClick={handleTestCredentials}
+              className="text-center py-2 w-full scale-100 text-shadow active:scale-95 bg-purple-800 text-white rounded-[4px]"
+            >
+              Sign in with Test Credentials
+            </button>
+          </div>
+        )}
         <p className="mt-10">
           <span className="text-gray-400 cursor-default">
             {isSignInForm ? "New to Netflix?" : "Already Registered?"}
